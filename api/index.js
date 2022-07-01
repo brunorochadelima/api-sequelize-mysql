5c862c5c58f5e2b99dbf05dc204fb7c3c800e0f9
Bruno Rocha de Lima <brunorocha33@live.com>
2022-07-01 13:36:14 -0300

    criando rotas

--- api/index.js
+++ api/index.js
@@ -1,17 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const routes = require('./routes');
+
 
 // iniciar express
 const app = express(); 
 
-// informa ao express que o body parse vai interceptar os dados e converter para json
-app.use(bodyParser.json()); 
+// importa rota
+routes(app)
 
 const port = 3000
-
-app.get('/teste', (req, res) => res
-.status(200)
-.send ({mensagem: 'boas vindas a API'}))
 
 // Ouvinte do express para ver se ta tudo ok com o servidor
 app.listen(port, () => console.log(`servidor está rodando na porta ${port}`)) 
