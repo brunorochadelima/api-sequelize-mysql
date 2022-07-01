d0e92eefb059310e739bbb127c08894a0f4372a5
Bruno Rocha de Lima <brunorocha33@live.com>
2022-07-01 16:41:08 -0300

    Métodos de CRUD

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -3,9 +3,23 @@ const database = require("../models");
 class PessoaController {
   static async pegaTodasAsPessoas(req, res) {
     try {
-      const todasAsPessoas = await database.Pessoas.findAll(); //Find all é p mesmo que select * from pessoas
+      const todasAsPessoas = await database.Pessoas.findAll(); //Find all é mesmo que select * from pessoas
       return res.status(200).json(todasAsPessoas);
     } catch (error) {
+      res.status(500).json(error.message);
+    }
+  }
+
+  static async pegaUmaPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      const umaPessoa = await database.Pessoas.findOne({
+        where: {
+          id: Number(id),
+        },
+      });
+      return res.status(200).json(umaPessoa);
+    } catch {
       res.status(500).json(error.message);
     }
   }
