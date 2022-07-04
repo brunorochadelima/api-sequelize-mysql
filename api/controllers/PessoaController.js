86dd1465b4e0fddc1bedb145ae4444aa7077f0ab
Bruno Rocha de Lima <brunorocha33@live.com>
2022-07-04 09:12:27 -0300

    método deleta pessoa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -42,10 +42,22 @@ class PessoaController {
          const pessoaAtualizada = await database.Pessoas.findOne({ where: { id: Number(id)},
         });
         return res.status(200).json(pessoaAtualizada)
-    } catch {
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+  }
+
+  static async deletaPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      await database.Pessoas.destroy({where:{ id: Number(id)}})
+      res.status(200).json({messagem: `id ${id} deletado com sucesso!`});
+    } catch (error) {
       res.status(500).json(error.message);
     }
   }
 }
+
+
 
 module.exports = PessoaController;
