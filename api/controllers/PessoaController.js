097ecfed4a17cbbc75bc7d327f44abf9884d7836
Bruno Rocha de Lima <brunorocha33@live.com>
2022-07-11 09:02:19 -0300

    Lendo uma matrícula

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -28,20 +28,21 @@ class PessoaController {
     const novaPessoa = req.body;
     try {
       const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
-      return res.status(200).json(novaPessoaCriada)
+      return res.status(200).json(novaPessoaCriada);
     } catch (error) {
       res.status(500).json(error.message);
     }
   }
 
   static async atualizaPessoa(req, res) {
-    const novasInfos = req.body
+    const novasInfos = req.body;
     const { id } = req.params;
-    try { 
-         await database.Pessoas.update(novasInfos, {where:{ id: Number(id)}})
-         const pessoaAtualizada = await database.Pessoas.findOne({ where: { id: Number(id)},
-        });
-        return res.status(200).json(pessoaAtualizada)
+    try {
+      await database.Pessoas.update(novasInfos, { where: { id: Number(id) } });
+      const pessoaAtualizada = await database.Pessoas.findOne({
+        where: { id: Number(id) },
+      });
+      return res.status(200).json(pessoaAtualizada);
     } catch (error) {
       res.status(500).json(error.message);
     }
@@ -50,14 +51,29 @@ class PessoaController {
   static async deletaPessoa(req, res) {
     const { id } = req.params;
     try {
-      await database.Pessoas.destroy({where:{ id: Number(id)}})
-      res.status(200).json({messagem: `id ${id} deletado com sucesso!`});
+      await database.Pessoas.destroy({ where: { id: Number(id) } });
+      res.status(200).json({ messagem: `id ${id} deletado com sucesso!` });
     } catch (error) {
       res.status(500).json(error.message);
     }
   }
+
+  //Matrícula
+
+  static async pegaUmaMatricula(req, res) {
+    const { estudanteId, matriculaId } = req.params;
+    try {
+      const umaMatricula = await database.Matriculas.findOne({
+        where: {
+          id: Number(matriculaId),
+          estudante_id: Number(estudanteId),
+        },
+      });
+      return res.status(200).json(umaMatricula);
+    } catch {
+      res.status(500).json(error.message);
+    }
+  }
 }
-
-
 
 module.exports = PessoaController;
